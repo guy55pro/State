9161b62b1ea1310c32cd86ac90be6b87fc53de94
MahdiKrimi <mahdi-smile@hotmail.com>
2021-06-10 18:00:42 -0700

    miaou

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import { getSuggestedQuery } from '@testing-library/dom';
+import React,{ Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component {
+   
+  
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      fullName: "",
+      bio: "",
+      profession: '',
+      isOn: false
+    };
+  }
+
+  profile = {
+    fullName: "FULLNAME: Mahdi krimi",
+    bio: "BIO: Im cool",
+    imgSrc: "./me.jpg",
+    profession: 'PROFESSION: Student',
+    isOn: false
+  }
+
+  update = () =>  {if(this.state.isOn === false)
+    {this.setState({...this.profile, isOn: true, button:"isOn"});}
+    else {this.setState({...this.state})}
+    }
+  
+  gon(){
+    this.setState({
+      fullName: "CODENAME: Cicada 3301"
+    })
+  }
+   
+  render() {
+    return (
+      <div>
+        <h1>{this.state.fullName}</h1>
+        <h1>{this.state.bio}</h1>
+        <button onClick={() => this.gon()}> Click </button>
+        <h3>{this.state.profession}</h3>
+        <button onClick={this.update}>{this.state.isOn}Show</button>
+        <button onClick={this.update2}>{this.state.isOn}Hide</button>
+        <img src={this.state.imgSrc}/>
+      </div>
+
+      
+    )
+    
+  }
 }
-
-export default App;
